d04dfcd2d200caaf07c21da13f49b6afe7650bb6
Arzutuyan <kurdalem@gmail.com>
2015-08-18 17:31:34 +0300

    4

---
+++ gun4/veriler.php
@@ -0,0 +1,16 @@
+<?php
+
+        $uniler =[];
+        $uniler[]="balıkesir unıversitesi";
+        $uniler[]="AİBU unıversitesi";
+        $uniler[]="yalova unıversitesi";
+        $uniler[]="istanbul unıversitesi";
+
+    $hobiler=[];
+    $hobiler[] ="okçuluk";
+    $hobiler[] ="mangal";
+    $hobiler[] ="yüzme";
+    $hobiler[] ="kitap okuma";
+    $hobiler[] ="iş makinesi izleme";
+
+    ?>
